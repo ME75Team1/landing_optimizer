28a7d4f0d623ee50afa78049d96b7dfcaa420b1b
Edju03 <90040359+Edju03@users.noreply.github.com>
2024-05-05 04:33:02 -0700

    Random Forest

--- scripts/PythonLandingROS.py
+++ scripts/PythonLandingROS.py
@@ -16,188 +16,12 @@ import ros_numpy
 import std_msgs.msg
 from sklearn.ensemble import RandomForestRegressor  # Import Random Forest model
 
-class tree(object):
-    """
-    Compute the score of query points based on the scores of their k-nearest neighbours,
-    weighted by the inverse of their distances.
-
-    @reference:
-    https://en.wikipedia.org/wiki/Inverse_distance_weighting
-
-    Arguments:
-    ----------
-        X: (N, d) ndarray
-            Coordinates of N sample points in a d-dimensional space.
-        z: (N,) ndarray
-            Corresponding scores.
-        leafsize: int (default 10)
-            Leafsize of KD-tree data structure;
-            should be less than 20.
-
-    Returns:
-    --------
-        tree instance: object
-
-    Example:
-    --------
-
-    # 'train'
-    idw_tree = tree(X1, z1)
-
-    # 'test'
-    spacing = np.linspace(-5., 5., 100)
-    X2 = np.meshgrid(spacing, spacing)
-    X2 = np.reshape(X2, (2, -1)).T
-    z2 = idw_tree(X2)
-
-    See also:
-    ---------
-    demo()
-
-    """
-
-    
-    # See https://github.com/paulbrodersen/inverse_distance_weighting
-    # for the original GitHub repository containing this class
-    
-    # This class uses the below license:
-
-    # BEGIN LICENSE
-    # Copyright (C) 2016 Paul Brodersen <paulbrodersen+idw@gmail.com>
-
-    # Author: Paul Brodersen <paulbrodersen+idw@gmail.com>
-    
-    # This program is free software; you can redistribute it and/or
-    # modify it under the terms of the GNU General Public License
-    # as published by the Free Software Foundation; either version 3
-    # of the License, or (at your option) any later version.
-    
-    # This program is distributed in the hope that it will be useful,
-    # but WITHOUT ANY WARRANTY; without even the implied warranty of
-    # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    # GNU General Public License for more details.
-    
-    # You should have received a copy of the GNU General Public License
-    # along with this program. If not, see <http://www.gnu.org/licenses/>.
-    # END LICENSE
-    
-    def __init__(self, X=None, z=None, leafsize=10):
-        if not X is None:
-            self.tree = cKDTree(X, leafsize=leafsize )
-        if not z is None:
-            self.z = np.array(z)
-
-    def fit(self, X=None, z=None, leafsize=10):
-        """
-        Instantiate KDtree for fast query of k-nearest neighbour distances.
-
-        Arguments:
-        ----------
-            X: (N, d) ndarray
-                Coordinates of N sample points in a d-dimensional space.
-            z: (N,) ndarray
-                Corresponding scores.
-            leafsize: int (default 10)
-                Leafsize of KD-tree data structure;
-                should be less than 20.
-
-        Returns:
-        --------
-            idw_tree instance: object
-
-        Notes:
-        -------
-        Wrapper around __init__().
-
-        """
-        return self.__init__(X, z, leafsize)
-
-    def __call__(self, X, k=3, eps=1e-6, p=2, regularize_by=1e-9):
-        """
-        Compute the score of query points based on the scores of their k-nearest neighbours,
-        weighted by the inverse of their distances.
-
-        Arguments:
-        ----------
-            X: (N, d) ndarray
-                Coordinates of N query points in a d-dimensional space.
-
-            k: int (default 6)
-                Number of nearest neighbours to use.
-
-            p: int or inf
-                Which Minkowski p-norm to use.
-                1 is the sum-of-absolute-values "Manhattan" distance
-                2 is the usual Euclidean distance
-                infinity is the maximum-coordinate-difference distance
-
-            eps: float (default 1e-6)
-                Return approximate nearest neighbors; the k-th returned value
-                is guaranteed to be no further than (1+eps) times the
-                distance to the real k-th nearest neighbor.
-
-            regularise_by: float (default 1e-9)
-                Regularise distances to prevent division by zero
-                for sample points with the same location as query points.
-
-        Returns:
-        --------
-            z: (N,) ndarray
-                Corresponding scores.
-        """
-        self.distances, self.idx = self.tree.query(X, k, eps=eps, p=p)
-        self.distances += regularize_by
-        weights = self.z[self.idx.ravel()].reshape(self.idx.shape)
-        mw = np.sum(weights/self.distances, axis=1) / np.sum(1./self.distances, axis=1)
-        return mw
-
-    def transform(self, X, k=3, p=2, eps=1e-6, regularize_by=1e-9):
-        """
-        Compute the score of query points based on the scores of their k-nearest neighbours,
-        weighted by the inverse of their distances.
-
-        Arguments:
-        ----------
-            X: (N, d) ndarray
-                Coordinates of N query points in a d-dimensional space.
-
-            k: int (default 6)
-                Number of nearest neighbours to use.
-
-            p: int or inf
-                Which Minkowski p-norm to use.
-                1 is the sum-of-absolute-values "Manhattan" distance
-                2 is the usual Euclidean distance
-                infinity is the maximum-coordinate-difference distance
-
-            eps: float (default 1e-6)
-                Return approximate nearest neighbors; the k-th returned value
-                is guaranteed to be no further than (1+eps) times the
-                distance to the real k-th nearest neighbor.
-
-            regularise_by: float (default 1e-9)
-                Regularise distances to prevent division by zero
-                for sample points with the same location as query points.
-
-        Returns:
-        --------
-            z: (N,) ndarray
-                Corresponding scores.
-
-        Notes:
-        ------
-
-        Wrapper around __call__().
-        """
-        return self.__call__(X, k, eps, p, regularize_by)
-
-
 def train_random_forest(X_train, Y_train):
     rf = RandomForestRegressor(n_estimators=100)  # You can adjust the number of trees (n_estimators) as needed
     rf.fit(X_train, Y_train)
     return rf
 
-def optimizer(ground_pcl, distance_to_top_leg, distance_to_right_leg, resolution):
+def optimizer(ground_pcl, distance_to_top_leg, distance_to_right_leg, resolution, rf):
     # Convert ground point cloud to digital terrain model
     grid_size = [distance_to_right_leg/resolution[0], distance_to_top_leg/resolution[1]]
     x_min, x_max = np.min(ground_pcl[:, 0]), np.max(ground_pcl[:, 0])
@@ -206,13 +30,8 @@ def optimizer(ground_pcl, distance_to_top_leg, distance_to_right_leg, resolution
     yGrid = np.arange(y_min, y_max + grid_size[1], grid_size[1])
     XGrid, YGrid = np.meshgrid(xGrid, yGrid)
 
-    # Train the random forest model
-    X_train = ground_pcl[:, :2]  # Assuming the first two columns are x and y coordinates
-    Y_train = ground_pcl[:, 2]   # Assuming the third column is the z-coordinate (terrain height)
-    rf_model = train_random_forest(X_train, Y_train)
-
     # Predict terrain height using the trained random forest
-    terrainModel = rf_model.predict(np.column_stack((XGrid.ravel(), YGrid.ravel()))).reshape(XGrid.shape)
+    terrainModel = rf.predict(np.column_stack((XGrid.ravel(), YGrid.ravel()))).reshape(XGrid.shape)
 
     # Calculate points related to leg positions
     distance_to_top_leg_points = int(distance_to_top_leg/grid_size[1])
